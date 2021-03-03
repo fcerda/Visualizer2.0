fbf4ba256fb58e4481701f8583941cd4c80e971b
danibarker <caseybarker86@gmail.com>
2021-03-02 19:52:29 -0700

    splitting app and server for easier testing

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const cors = require('cors')
-const app = express()
-//this before routes to enable cors for all routes
-app.use(cors())
+
+
+const app = require("./app");
+
+require('dotenv').config()
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
